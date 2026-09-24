03c8db7f70191db04aa888a242931e877d7856fc
Manuelly03 <fernandesmanuelly341@gmail.com>
2026-09-24 01:07:05 -0300

    feat: add api root rout

--- src/app.js
+++ src/app.js
@@ -3,4 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+
+app.get('/', (req, res) => {
+    res.json({
+        mensagem: 'API de Agendamento para Salão de Beleza'
+    });
+});
 export default app;
