bfb0dede1526c938376a40768048305402477c89
Manuelly03 <fernandesmanuelly341@gmail.com>
2026-09-24 01:32:48 -0300

    feat: add client creation controller

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -3,3 +3,14 @@ import clientes from '../data/clientes.js';
 export function listarClientes(req, res) {
     res.json(clientes);   
 }
+
+export function criarCliente(req, res) {
+    const novoCliente = {
+        id: clientes.length + 1,
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        email: req.body.email        
+    };
+    clientes.push(novoCliente);
+    res.status(201).json(novoCliente);      
+}
