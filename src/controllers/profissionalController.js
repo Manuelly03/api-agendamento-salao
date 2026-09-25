99fd9e61778165f74fd0cbd3d2974d0ccc79927b
Manuelly03 <fernandesmanuelly341@gmail.com>
2026-09-25 00:00:44 -0300

    feat: add professional creation controller

--- src/controllers/profissionalController.js
+++ src/controllers/profissionalController.js
@@ -3,3 +3,12 @@ import profissionais from '../data/profissionais.js';
 export function listarProfissionais(req, res) {
     res.json(profissionais);
 }
+export function criarProfissional(req, res) {
+    const novoProfissional = {
+    id: profissionais.length + 1,
+    nome: req.body.nome,
+    especialidade: req.body.especialidade
+};
+    profissionais.push(novoProfissional);
+res.status(201).json(novoProfissional);
+}
