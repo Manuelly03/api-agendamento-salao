fafabf642df8cc1e466ab05d41f2c45657a94e6d
Manuelly03 <fernandesmanuelly341@gmail.com>
2026-09-25 00:05:56 -0300

    feat: add professional search route

--- src/routes/profissionalRoutes.js
+++ src/routes/profissionalRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { listarProfissionais,
-         criarProfissional
+         criarProfissional,
+        buscarProfissionalPorId
+
  } from '../controllers/profissionalController.js';
 
 const router = express.Router();
 
 router.get('/profissionais', listarProfissionais);
 router.post('/profissionais', criarProfissional);
-
+router.get('/profissionais/:id', buscarProfissionalPorId);
 
 export default router;
