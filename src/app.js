edc72a7762d1a4fb85818048e20625d043bed770
Manuelly03 <fernandesmanuelly341@gmail.com>
2026-09-24 23:54:27 -0300

    feat: connect professional routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from  'express';
 import clienteRoutes from './routes/clienteRoutes.js';
+import profissionalRoutes from './routes/profissionalRoutes.js';
 const app = express();
 
 app.use(express.json());
 app.use(clienteRoutes);
+app.use(profissionalRoutes);
 
 app.get('/', (req, res) => {
     res.json({
